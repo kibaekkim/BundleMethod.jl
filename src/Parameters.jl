134207fa7ef25d28ce2343735e1cde362b70176c
Hideaki Nakao <h.nakao1992blanca@gmail.com>
2021-10-05 06:53:58 -0500

    added time limit

--- src/Parameters.jl
+++ src/Parameters.jl
@@ -2,6 +2,7 @@ Base.@kwdef mutable struct Parameters
     maxiter::Int = 3000     # maximum number of iterations
     ncuts_per_iter::Int = 1 # number of cuts added per iteration
     obj_limit::Float64 = -Inf # termination condition
+    time_limit::Float64 = 3600.0
 
     ϵ_s::Float64 = 1.e-5 # termination tolerance
     m_L::Float64 = 1.e-4 # serious step condition parameter (0, 0.5)
