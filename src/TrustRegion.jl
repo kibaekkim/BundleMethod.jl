c1eaf817d3d8c9ad37de6a4da1c5201b10782274
Hideaki Nakao <h.nakao1992blanca@gmail.com>
2021-10-12 09:23:24 -0500

    update iis

--- src/TrustRegion.jl
+++ src/TrustRegion.jl
@@ -171,7 +171,7 @@ function collect_model_solution!(method::TrustRegionMethod)
         JuMP.compute_conflict!(model)
         for (type1, type2) in JuMP.list_of_constraint_types(model)
             for constr in JuMP.all_constraints(model, type1, type2)
-                MOI.get(model, MOI.ConstraintConflictStatus(), constr)
+                println(MOI.get(model, MOI.ConstraintConflictStatus(), constr))
             end
         end
     end
